0933a716b234f0b5c31c923e60108d30ec7a7473
mredgar2004 <edgarzombie80@gmail.com>
2025-07-11 12:10:28 +0300

    Create 📄 config.js
    Signed-off-by: mredgar2004 <edgarzombie80@gmail.com>

---
+++ 📄 config.js
@@ -0,0 +1,9 @@
+```javascript
+module.exports = {
+  prefix: '!',
+  ownerNumber: '255625501821',
+  botName: 'MR....EDGAR-XMD',
+  ownerName: 'MR. EDGAR',
+  footer: '© MR....EDGAR-XMD Bot',
+};
+```
